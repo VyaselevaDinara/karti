7e06489cfc1280b4a687d032157811bec6e67ff2
Вяселева Динара <118040854+VyaselevaDinara@users.noreply.github.com>
2023-12-18 13:23:40 +0300

    Add files via upload

---
+++ karti.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        HttpClient client = new();
+
+        Console.WriteLine("Введите ссылку для скачивания картинки: ");
+        string nameWebsite = Console.ReadLine();
+
+        HttpResponseMessage response = await client.GetAsync(nameWebsite);
+        byte[] data = await response.Content.ReadAsByteArrayAsync();
+
+        await File.WriteAllBytesAsync("C:\\Users\\Nitro 5\\.git", data);
+
+        Process.Start(new ProcessStartInfo { FileName = "C:\\Users\\Nitro 5\\.git", UseShellExecute = true });
+    }
+}
